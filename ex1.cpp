d85aa2788ad3da04172c7ab75f2055134410cc3c
tithi20i <116013911+tithi20i@users.noreply.github.com>
2022-10-24 23:35:02 +0600

    Add files via upload
    lab task 1

---
+++ ex1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+// 12 32 43 1 54 53 15 64 3 13
+
+
+
+int main(){
+
+    int a[10]={12,32,43,1,54,53,15,64,3,13};
+
+    for(int i=0;i<10;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+    for(int i=9;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+
+
+return 0;
+}
